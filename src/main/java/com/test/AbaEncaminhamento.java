a38d1ef17c141ad028a6923dcc6f3c039b0e10ab
Renato Alves Felix <rfelix@redhat.com>
2020-09-14 11:03:22 -0300

    [RedHat] solucao 03 reprodutor

--- src/main/java/com/test/AbaEncaminhamento.java
+++ src/main/java/com/test/AbaEncaminhamento.java
@@ -1,15 +1,13 @@
 package com.test;
 
-import javax.inject.Inject;
 import java.io.Serializable;
 import java.util.logging.Logger;
 
 public class AbaEncaminhamento implements ObservadorEncaminhamentoDto, Serializable {
     private static final Logger LOGGER = Logger.getLogger(AbaEncaminhamento.class.getName());
 
-    @Inject
-    public AbaEncaminhamento(ObserverEncaminhamentoDto observerEncaminhamentoDto) {
-        observerEncaminhamentoDto.registrarObservador(this);
+    public AbaEncaminhamento() {
+        LOGGER.info("[constructor] AbaEncaminhamento (this) =" + this);
     }
 
     @Override
